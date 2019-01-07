357300a7043df418be2944eb68c8551a3e541742
Amoun123 <31475446+Amoun123@users.noreply.github.com>
2019-01-07 13:46:22 +0100

    Add files via upload

---
+++ src/app/interfaces/marsupilamis.ts
@@ -0,0 +1,12 @@
+export interface Marsupilamis {
+
+  _id?: String;
+  Nom: String;
+  famille: String;
+  age: String;
+  race: String;
+  nourriture: String;
+  username:  String;
+  pass:  String;
+
+}
